a43959d6dd823a3ce28a2a12cb83ffa7796cc050
juanmcortez <juanm.cortez@gmail.com>
2024-09-23 15:57:21 -0300

    :construction: User email on its own model.

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'username',
         'password',
+        'remember_token',
     ];
 
     /**
@@ -39,7 +40,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
