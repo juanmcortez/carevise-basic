54fee32879c0e32311d888cc4b712fd03346873f
juanmcortez <juanm.cortez@gmail.com>
2024-09-25 17:38:04 -0300

    🚧 Auth pages and urls

--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Users\UserController;
 use App\Http\Controllers\Main\DashboardController;
 
-Route::get('/', DashboardController::class)
-    ->name('dashboard');
+// Routes are accesible if the user is logged in
+Route::middleware(['auth'])->group(function () {
+    // Dashboard route
+    Route::get('/', DashboardController::class)->name('dashboard');
 
-Route::controller(UserController::class)
-    ->name('user.')
-    ->group(function (){
-        // Logged-in user routes
-        Route::get('/user/edit', 'index')
-            ->name('self.edit');
-        // Logged-in user routes
+    // Users routes
+    Route::controller(UserController::class)
+        ->name('user.')
+        ->group(function () {
+            // Logged-in user routes
+            Route::get('/user/edit', 'index')->name('self.edit');
+            // Logged-in user routes
 
-        // System user routes
-        Route::get('/users/list', 'show')
-            ->name('list');
-        Route::get('/users/{user}/profile/edit', 'edit')
-            ->name('profile.edit');
-        // System user routes
+            // System user routes
+            Route::get('/users/list', 'show')->name('list');
+            Route::get('/users/{user}/profile/edit', 'edit')->name('profile.edit');
+            // System user routes
 
-        // Common user routes
-        Route::patch('/users/update', 'update')
-            ->name('update');
-        // Common user routes
+            // Common user routes
+            Route::patch('/users/update', 'update')->name('update');
+            // Common user routes
+        });
 });
