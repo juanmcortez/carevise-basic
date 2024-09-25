5594487237f383b8e2b0f0b7c99ac63592eed649
juanmcortez <juanm.cortez@gmail.com>
2024-09-25 00:55:11 -0300

    🔧 Config changes

--- config/mail.php
+++ config/mail.php
@@ -109,8 +109,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'administration@carevise-basic.com'),
+        'name' => env('MAIL_FROM_NAME', 'Administration Department'),
     ],
 
 ];
