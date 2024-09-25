27e65f16fc8f03421e03f1a2cbe0095eca68bdd8
juanmcortez <juanm.cortez@gmail.com>
2024-09-25 10:56:50 -0300

    🚧 Users seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
+            'username' => 'test.user',
         ]);
+
+        User::factory(14)->create();
     }
 }
