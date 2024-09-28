45f095d9be368c4462409d8a65f95a6c3f93d21a
juanmcortez <juanm.cortez@gmail.com>
2024-09-28 11:14:11 -0300

    🚧 New empty fields function in factory

--- database/factories/Commons/PhoneFactory.php
+++ database/factories/Commons/PhoneFactory.php
@@ -12,10 +12,21 @@ class PhoneFactory extends Factory
     public function definition(): array
     {
         return [
-            'country_code'  => '+1',
-            'area_code'     => $this->faker->randomNumber(3, true),
+            'country_code' => '+1',
+            'area_code' => $this->faker->randomNumber(3, true),
             'prefix_number' => $this->faker->randomNumber(3, true),
-            'line_number'   => $this->faker->randomNumber(4, true),
+            'line_number' => $this->faker->randomNumber(4, true),
         ];
+    }
+
+    public function withEmptyFields()
+    {
+        return $this->state(function (array $attributes) {
+            $fields = ['country_code', 'area_code', 'prefix_number', 'line_number'];
+            foreach ($fields as $field) {
+                $attributes[$field] = null;
+            }
+            return $attributes;
+        });
     }
 }
